7e3ed768bcbe6871dccfaa446be8dccfb6fd133c
David Yuan <Summer_xx@163.com>
2020-06-29 22:42:58 +0800

    有Bug

---
+++ src/main/java/com/imooc/service/impl/ProductServiceImpl.java
@@ -0,0 +1,42 @@
+package com.imooc.service.impl;
+
+import com.imooc.dataobject.ProductInfo;
+import com.imooc.enums.ProductStatusEnum;
+import com.imooc.repository.ProductInfoRepository;
+import com.imooc.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
+public class ProductServiceImpl implements ProductService {
+
+    @Autowired
+    private ProductInfoRepository repository;
+
+    @Override
+    public ProductInfo findOne(String productId) {
+        return repository.findById(productId).orElse(null);
+    }
+
+    @Override
+    public List<ProductInfo> findUpAll() {
+        return null;
+    }
+
+    @Override
+    public List<ProductInfo> findAll() {
+        return repository.findByProductStatus(ProductStatusEnum.UP.getCode());
+    }
+
+    @Override
+    public Page<ProductInfo> findAll(Pageable pageable) {
+        return repository.findAll(pageable);
+    }
+
+    @Override
+    public ProductInfo save(ProductInfo productInfo) {
+        return repository.save(productInfo);
+    }
+}
---
+++ src/test/java/com/imooc/repository/ProductInfoRepositoryTest.java
@@ -0,0 +1,59 @@
+package com.imooc.repository;
+
+import com.imooc.dataobject.ProductInfo;
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+class ProductInfoRepositoryTest {
+    @Autowired
+    private ProductInfoRepository repository;
+
+    @Test
+    public void SaveTest() {
+        ProductInfo productInfo = new ProductInfo();
+        productInfo.setProductId("123456");
+        productInfo.setProductName("皮蛋粥");
+        productInfo.setProductPrice(new BigDecimal(3.2));
+        productInfo.setProductStock(100);
+        productInfo.setProductDescription("很好喝的粥");
+        productInfo.setProductIcon("http://xxxx.j[g");
+        productInfo.setProductStatus(0);
+        productInfo.setCatrgoryType(2);
+        ProductInfo result = repository.save(productInfo);
+        Assert.assertNotNull(result);
+    }
+
+    @Test
+    public void findByProductStatus() throws Exception {
+        List<ProductInfo> productInfoList = repository.findByProductStatus(0);
+        Assert.assertNotEquals(0,productInfoList.size());
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
